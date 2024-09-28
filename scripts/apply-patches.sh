ca0d622395fdcd842ca606a1b9fa13dd4daa95f2
InkoHX <me@inkohx.dev>
2024-09-28 15:37:00 +0900

    fix `apply-patches.sh`

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
-find ./patches -name '*.patch' -exec git apply {} \
+find ./patches -name '*.patch' -print0 -type f | while IFS= read -r -d '' file; do
+  echo "Applying patch $file..."
+  git apply "$file"
+  echo "Patch $file applied."
+done
